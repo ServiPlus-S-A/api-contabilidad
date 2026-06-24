715a6f44d1c1b23eb064488f2bbf78118213d963
Manuel Rodriguez <150725421+ManuelR12@users.noreply.github.com>
2026-06-23 23:18:56 -0500

    fix: prettier, container names collision and dev deploy dir

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -44,7 +44,9 @@ export default function LoginPage() {
               style={styles.input}
               type="text"
               value={username}
-              onChange={(e) => { setUsername(e.target.value); }}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
               placeholder="nombre.usuario"
               required
               autoFocus
@@ -57,7 +59,9 @@ export default function LoginPage() {
               style={styles.input}
               type="password"
               value={password}
-              onChange={(e) => { setPassword(e.target.value); }}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               placeholder="••••••••"
               required
             />
